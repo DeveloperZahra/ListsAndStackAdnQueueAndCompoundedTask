fe698f6a737e692438965107c056b98580f5d3c9
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-04-16 11:11:06 +0400

    Frequent Numbers

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers = new List<int> { 50, 10, 50, 12, 10, 30, 50, 20, 12 };
+            int N = 3;
+            var topNFrequent = numbers
+             .GroupBy(x => x)
+            .Select(g => new { number = g.Key, Count = g.Count() })
+            .OrderByDescending(g => g.Count)
+            .Take(N)
+            .ToList();
+
+            Console.WriteLine($"Top {N} frequent numbers:");
+            foreach (var item in topNFrequent)
+            {
+                Console.WriteLine($"Number: {item.number}, Count: {item.Count}");
+            }
         }
     }
 }
